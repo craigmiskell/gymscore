32b015b3620f570bac6408359c43b820b1a75d31
Craig Miskell <craig@stroppykitten.com>
2022-05-01 16:13:45 +1200

    Hello world
    From https://www.electronjs.org/docs/latest/tutorial/quick-start, to give a framework for starting dev

---
+++ main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow} = require('electron');
+const path = require('path')
+const isDev = require('electron-is-dev');
+
+let mainWindow; 
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+  // TODO: we'll add a menu again later when we have a need
+  win.setMenu(null);
+  if(isDev) { 
+    win.webContents.openDevTools();
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  // Recommended boilerplate to recreate a window when activated
+  // which gives the expected UX on OSX
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+});
+
+// Recommended boiler-plate to quit the app when all windows are closed
+// except for OSX (see on activate above)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
