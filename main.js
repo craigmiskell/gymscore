2bb0f8e7a9639badcd31823947576f37a212e67a
Craig Miskell <craig@stroppykitten.com>
2022-05-07 09:19:45 +1200

    Fix eslintrc for nodejs, and apply fixes

--- main.js
+++ main.js
@@ -1,38 +1,36 @@
-const { app, BrowserWindow} = require('electron');
-const path = require('path')
-const isDev = require('electron-is-dev');
-
-let mainWindow; 
+const { app, BrowserWindow} = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev");
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 1024,
     height: 768,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, "preload.js")
     }
-  })
+  });
 
-  win.loadFile('index.html')
+  win.loadFile("index.html");
   // TODO: we'll add a menu again later when we have a need
   win.setMenu(null);
-  if(isDev) { 
+  if(isDev) {
     win.webContents.openDevTools();
   }
-}
+};
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
   // Recommended boilerplate to recreate a window when activated
   // which gives the expected UX on OSX
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {createWindow();}
+  });
 });
 
 // Recommended boiler-plate to quit the app when all windows are closed
 // except for OSX (see on activate above)
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {app.quit();}
+});
